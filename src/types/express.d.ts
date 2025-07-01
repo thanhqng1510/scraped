d5cc7f051a781d3ab54af9fc7ff966a0f5a17707
Thanh Nguyen <thanhqng1510@gmail.com>
2025-07-01 23:44:03 +0700

    create first DB migration

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,13 +1,9 @@
 import { User } from '@prisma/client';
 
-// to make the file a module and avoid the TypeScript error
-export {};
-
 declare global {
   namespace Express {
-    export interface Request {
+    interface Request {
       user?: User;
     }
   }
 }
-
